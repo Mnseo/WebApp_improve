57664a605129f362a1fd17dbf2ca94b081f54788
Victor/Minseo <100370200+MinseoShindor@users.noreply.github.com>
2022-05-19 17:59:49 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vue from 'vue'
+import App from './App.vue'
+
+
+
+library.add(faUserSecret)
+Vue.component(
+  'font-awesome-icon', FontAwesomeIcon
+)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
